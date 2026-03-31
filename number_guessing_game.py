809ebeb42db96de9f90cfaf179c3cdd87f3e5323
nkyaw9 <nyaingpk@gmail.com>
2026-03-31 01:09:49 -0400

    added comments

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -1,33 +1,33 @@
 import random
 
 def main():
-# Gets the range from user
+
     while True:
         try:
-            lowerBound = int(input("Lower bound: "))
-            upperBound = int(input("Upper bound: "))
-            if lowerBound == upperBound:
+            # Gets the range from user
+            lowerBound = int(input("Lower bound: ")) 
+            upperBound = int(input("Upper bound: ")) 
+            if lowerBound == upperBound: # invalid if user type same bounds
                 raise Exception("Lower and upper bound cannot be the same")
             break
-        except ValueError:
+        except ValueError: # invalid if its not an integer
             print("Please type in an integer value")
         except Exception as e:
             print(e)
 
-
+    # random number in the range
     number = random.randrange(lowerBound, upperBound + 1)
-    counter = 1
-
+    counter = 1 # track number of guesses
 
     while True:
         try:
+            # takes in user's guess
             guess = int(input(f"Guess {counter}: "))
             counter += 1
-           
-        except ValueError:
+        except ValueError: # nvalid if its not an integer
             print("Please type in an integer value")
 
-
+        # calculate high and low distance from the number
         result = prox(number, guess, lowerBound, upperBound)
         if result == 1:
             print("Correct!")
@@ -35,11 +35,8 @@ def main():
             break
         print(result)
 
-
-
-
+# calculate high and low distance from the number
 def prox(number, guess, lowerBound, upperBound):
-
 
     if guess < lowerBound or guess > upperBound:
         return "Out of range"
@@ -48,6 +45,6 @@ def prox(number, guess, lowerBound, upperBound):
     elif guess < number:
         return "Too low"
    
-    return 1
+    return 1 # if user guess correct
 
 main()
